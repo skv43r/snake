6d732f28f87bca9dbbd00e999a7cca2855e04c69
Alexandr <skv43r@gmail.com>
2024-10-07 15:58:33 +0300

    added game over function and counter update

--- scoreboard.py
+++ scoreboard.py
@@ -7,34 +7,30 @@
 
 import turtle
 
-window = turtle.Screen()
-window.setup(width=600, height=600)
-window.bgcolor('black')
-window.title('My Snake Game')
+# window = turtle.Screen()
+# window.setup(width=600, height=600)
+# window.bgcolor('black')
+# window.title('My Snake Game')
 
 class Scoreboard(turtle.Turtle):
-    def __init__(self, position=(-298, 260)):
+    def __init__(self):
         super().__init__()
         self.speed(0)
         self.color('pink')
         self.penup()
         self.hideturtle()
-        self.setposition(position)
+        self.count = 0
 
         
-    def count(self, count=0, high_score=0):
+    def score(self):
+        self.clear()
+        self.setposition(-298, 280)
+        self.write(f'Score: {self.count}', move=True, align='left', font=('Arial', 12, 'bold',))
+    
+    def update_count(self):
+        self.count += 1
 
-        self.write(f'Score: {count}\nHigh Score: {high_score}', move=True, align='left', font=('Arial', 12, 'bold',))
-
-
-
-#turtle.hideturtle()
-#turtle.penup()
-#turtle.goto(-298, 280)
-#turtle.color('pink')
-#turtle.write(f'Score: {count}', move=True, align='left', font=('Arial', 12, 'bold',))
-
-score = Scoreboard()
-score.count()
-
-turtle.mainloop()  
+    def game_over(self):
+        self.setposition(-125, 0)
+        self.color('red')
+        self.write(f'GAME OVER ', move=True, align='left', font=('Arial', 30, 'bold',))
